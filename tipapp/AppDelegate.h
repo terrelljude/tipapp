0238904734dfedca87732aa12f87b9ff3a207028
Terrell-Jude Ilechie <terrelljudei@fb.com>
2020-06-23 18:00:28 -0400

    Initial Commit

---
+++ tipapp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipapp
+//
+//  Created by Terrell-Jude Ilechie on 6/23/20.
+//  Copyright © 2020 Terrell-Jude Ilechie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ tipapp/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  tipapp
+//
+//  Created by Terrell-Jude Ilechie on 6/23/20.
+//  Copyright © 2020 Terrell-Jude Ilechie. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
